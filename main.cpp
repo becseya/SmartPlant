a97d1d9e1644eca83c21217585d7bd1c0c5090f7
Akos Becsey <becsey.a@gmail.com>
2021-10-23 10:59:05 +0200

    Fake sensor for testing

--- main.cpp
+++ main.cpp
@@ -4,21 +4,50 @@
  */
 
 #include "app/mode_selector.hpp"
+#include "app/sensor.hpp"
+#include "app/utils/array.hpp"
+#include "app/utils/log.hpp"
 #include "mbed.h"
 
 using namespace SmartPlant;
+
+struct DummySensor : Sensor
+{
+    DummySensor()
+        : Sensor("DUMMY")
+    {}
+
+    bool init() final { return true; }
+    void update() final { LOG_SENSOR("%s", "This is a test"); }
+};
 
 // Hardware elements and other classes
 BusOut       modeLeds(LED1, LED2);
 ModeSelector modeSelector(PB_2, modeLeds);
 
+// Sensors
+DummySensor dummy;
+
+const auto sensors = make_array<Sensor*>( //
+    &dummy);
+
 // -------------------------------------------------- START OF MAIN ---------------------------------------------------
 
 int main()
 {
+    // init
+    LOG_DEBUG("Initializing %u sensors...", sensors.size())
+    for (auto& s : sensors) {
+        if (!s->init())
+            LOG("Failed to initialize %s!", s->getName())
+    }
+
     while (true) {
         // update
         modeSelector.update();
+        for (auto& s : sensors) {
+            s->update();
+        }
 
         // sleep
         modeSelector.sleep();
