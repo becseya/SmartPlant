6916d405dbfc5294d78e2ef6eb7d36d3ad00d21f
maryyyyyyaa <76183522+maryyyyyyaa@users.noreply.github.com>
2021-11-14 16:38:43 +0100

    add aggregation for SoilMoisture.hpp

--- app/sensors/SoilMoisture.hpp
+++ app/sensors/SoilMoisture.hpp
@@ -1,14 +1,19 @@
 #pragma once
 
+#include "../aggregation/ScalarAggregator.hpp"
 #include "../sensor.hpp"
 #include "mbed.h"
 
 namespace SmartPlant {
 namespace Sensors {
 
+using Aggregation::ScalarAggregator;
+
 class SoilMoisture : public Sensor
 {
   public:
+    ScalarAggregator aggregator;
+
     SoilMoisture(PinName pin);
 
     float readPercentage();
