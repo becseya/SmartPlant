aa0d1d1bd44ff298dbcc54300ecb6ae1636a3dff
Akos Becsey <becsey.a@gmail.com>
2021-11-15 22:23:50 +0100

    Format files

--- app/sensors/Si7021.cpp
+++ app/sensors/Si7021.cpp
@@ -1,16 +1,16 @@
 #include "Si7021.hpp"
 
 #include "../utils/log.hpp"
- 
-#define READ_TEMP        0xE0 
-#define READ_RH          0xE5 
-#define READ_ID1_1       0xFA
-#define READ_ID1_2       0x0F
-#define READ_ID2_1       0xFC
-#define READ_ID2_2       0xC9
-#define ADDR             0x80
-#define DEVICE_ID        0x15
-#define FREQ             100000
+
+#define READ_TEMP  0xE0
+#define READ_RH    0xE5
+#define READ_ID1_1 0xFA
+#define READ_ID1_2 0x0F
+#define READ_ID2_1 0xFC
+#define READ_ID2_2 0xC9
+#define ADDR       0x80
+#define DEVICE_ID  0x15
+#define FREQ       100000
 
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -37,56 +37,49 @@ bool Si7021::init()
 {
     return true;
 }
-    
-    // Check if the sensor is active and responding
 
-   bool Si7021::check()
+// Check if the sensor is active and responding
+
+bool Si7021::check()
 {
     tx_buff[0] = READ_ID2_1;
     tx_buff[1] = READ_ID2_2;
-    if(i2c.write(ADDR, (char*)tx_buff, 2) != 0) return 0;
-    if(i2c.read(ADDR, (char*)rx_buff, 8) != 0) return 0;
-    
-    if(rx_buff[0] == DEVICE_ID)
-        return true;
-    else return 0;
-}
+    if (i2c.write(ADDR, (char*)tx_buff, 2) != 0)
+        return 0;
+    if (i2c.read(ADDR, (char*)rx_buff, 8) != 0)
+        return 0;
 
+    if (rx_buff[0] == DEVICE_ID)
+        return true;
+    else
+        return 0;
+}
 
 void Si7021::update()
 {
-   measure();
-   LOG_SENSOR("", rhData, tData);
+    measure();
+    LOG_SENSOR("", rhData, tData);
 }
-
 
 bool Si7021::measure()
 {
     tx_buff[0] = READ_RH;
-    if(i2c.write(ADDR, (char*)tx_buff, 1) != 0) return 0;
-    if(i2c.read(ADDR, (char*)rx_buff, 2) != 0) return 0;
-    
+    if (i2c.write(ADDR, (char*)tx_buff, 1) != 0)
+        return 0;
+    if (i2c.read(ADDR, (char*)rx_buff, 2) != 0)
+        return 0;
+
     rhData = ((uint32_t)rx_buff[0] << 8) + (rx_buff[1] & 0xFC);
-    rhData = (((rhData) * 15625L) >> 13) - 6000;
-    
+    rhData = (((rhData)*15625L) >> 13) - 6000;
+
     tx_buff[0] = READ_TEMP;
-    if(i2c.write(ADDR, (char*)tx_buff, 1) != 0) return 0;
-    if(i2c.read(ADDR, (char*)rx_buff, 2) != 0) return 0;
-    
+    if (i2c.write(ADDR, (char*)tx_buff, 1) != 0)
+        return 0;
+    if (i2c.read(ADDR, (char*)rx_buff, 2) != 0)
+        return 0;
+
     tData = ((uint32_t)rx_buff[0] << 8) + (rx_buff[1] & 0xFC);
-    tData = (((tData) * 21965L) >> 13) - 46850;
-    
+    tData = (((tData)*21965L) >> 13) - 46850;
+
     return 1;
 }
- 
-
-    
-   
-    
-    
-
-    
-    
-    
-
- 
