2a7738694a38f489705132ad94fa73af2c08142c
Akos Becsey <becsey.a@gmail.com>
2022-01-17 08:56:22 +0100

    Remove extra sensors

--- main.cpp
+++ main.cpp
@@ -5,115 +5,61 @@
 
 #include "aggregation/Manager.hpp"
 #include "app/RGBLed.hpp"
-#include "app/mode_selector.hpp"
 #include "app/sensors/Brightness.hpp"
 #include "app/sensors/Gps.hpp"
 #include "app/sensors/MMA8451Q.hpp"
 #include "app/sensors/Si7021.hpp"
-#include "app/sensors/SoilMoisture.hpp"
-#include "app/sensors/TCS3472_I2C.hpp"
-#include "app/utils/BinaryBuilder.hpp"
 #include "app/utils/array.hpp"
 #include "app/utils/log.hpp"
 #include "app/utils/misc.hpp"
 #include "mbed.h"
 
 #define MAX_NUMBER_OF_GLOBAL_EVENTS 5
+#define SENSOR_UPDATE_PERIOD        2s
 
 using namespace SmartPlant;
-using GlobalEventQueue  = SmartPlant::EventQueue;
-using SensorDataBuilder = BinaryBuilder<32>;
-
-// fnction definitions
-void buildSensorData(SensorDataBuilder& builder);
+using GlobalEventQueue = SmartPlant::EventQueue;
 
 // global event queue
 GlobalEventQueue globalEvents(MAX_NUMBER_OF_GLOBAL_EVENTS);
 
 // Hardware elements and other classes
-I2C          i2cBus(PB_9, PB_8);
-BusOut       modeLeds(LED1, LED2);
-ModeSelector modeSelector(globalEvents, PB_2, modeLeds);
-RGBLed       rgbLed(PB_12, PA_12, PA_11);
+I2C    i2cBus(PB_9, PB_8);
+RGBLed rgbLed(PB_12, PA_12, PA_11);
 
 // Sensors
-Sensors::MMA8451Q     sAccelerometer(i2cBus, PA_14, globalEvents);
-Sensors::Brightness   sBrightness(PA_4);
-Sensors::SoilMoisture sSoilMoisture(PA_0);
-Sensors::Gps          sGps(PA_9, PA_10);
-Sensors::Si7021       sTempHum(i2cBus);
-Sensors::TCS3472_I2C  sColor(i2cBus);
+Sensors::Brightness sBrightness(PA_4);
+Sensors::Gps        sGps(PA_9, PA_10);
+Sensors::Si7021     sTempHum(i2cBus);
 
 const auto sensors = make_array<Sensor*>( //
-    &sAccelerometer,
     &sBrightness,
-    &sSoilMoisture,
     &sGps,
-    &sTempHum,
-    &sColor);
+    &sTempHum);
 
 // Aggregation
 const auto aggregators = make_array<Aggregation::Aggregator*>( //
-    &sAccelerometer.aggregatorX,
-    &sAccelerometer.aggregatorY,
-    &sAccelerometer.aggregatorZ,
     &sBrightness.aggregator,
-    &sSoilMoisture.aggregator,
     &sTempHum.aggregatorTemp,
-    &sTempHum.aggregatorHumidity,
-    &sColor.aggregator);
+    &sTempHum.aggregatorHumidity);
 
 Aggregation::Manager<array_size(aggregators)> aggregationManager(aggregators);
-SensorDataBuilder                             sensorDataBuilder;
-
-// --------------------------------------------------------------------------------------------------------------------
-
-void buildSensorData(SensorDataBuilder& builder)
-{
-    auto gps = sGps.getLastMeasurement();
-
-    builder.append<int16_t>(sAccelerometer.aggregatorX.getLastSample() * 1000); // Accelerometer - x
-    builder.append<int16_t>(sAccelerometer.aggregatorY.getLastSample() * 1000); // Accelerometer - y
-    builder.append<int16_t>(sAccelerometer.aggregatorZ.getLastSample() * 1000); // Accelerometer - z
-    builder.append<uint16_t>(sAccelerometer.getPositionChanges());              // Accelerometer - flips
-    builder.append<uint16_t>(sAccelerometer.getTapCount());                     // Accelerometer - taps
-    builder.append<int16_t>(sBrightness.aggregator.getLastSample() * 100);      // Brightness
-    builder.append<int16_t>(sSoilMoisture.aggregator.getLastSample() * 100);    // Soil moisture
-
-    if (gps.valid) {
-        builder.append<float>(gps.lat); // Gps - lat
-        builder.append<float>(gps.lon); // Gps - lon
-    } else {
-        builder.append<float>(0); // Gps - lat
-        builder.append<float>(0); // Gps - lon
-    }
-
-    builder.append<int16_t>(sTempHum.aggregatorTemp.getLastSample() * 100);     // Temp
-    builder.append<int16_t>(sTempHum.aggregatorHumidity.getLastSample() * 100); // Hum
-    builder.append<char>(colorToString(sColor.aggregator.getLastSample())[0]);  // Color
-    builder.append<char>(modeToStr(modeSelector.getMode())[0]);                 // Mode
-}
 
 // -------------------------------------------------- START OF MAIN ---------------------------------------------------
 
 int main()
 {
-    modeSelector.onTick([&](Mode currentMode) -> void {
-        LOG("") // extra new line
-        aggregationManager.update(currentMode == Mode::Normal);
-        for (auto& s : sensors)
-            s->update();
-
-        sensorDataBuilder.reset();
-        buildSensorData(sensorDataBuilder);
-    });
-
     // init
     LOG_DEBUG("Initializing %u sensors...", sensors.size())
     for (auto& s : sensors) {
         if (!s->init())
             LOG_ERROR("Failed to initialize %s!", s->getName())
     }
+
+    globalEvents.call_every(SENSOR_UPDATE_PERIOD, [&]() -> void {
+        for (auto& s : sensors)
+            s->update();
+    });
 
     // infinite loop
     globalEvents.dispatch_forever();
